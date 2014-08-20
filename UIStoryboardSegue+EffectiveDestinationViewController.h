32bc7d4f76477e59b47236d40deb963f13b0d071
Jonghwan Hyeon <hyeon0145@gmail.com>
2014-08-20 16:29:53 +0900

    Add file comment

--- UIStoryboardSegue+EffectiveDestinationViewController.h
+++ UIStoryboardSegue+EffectiveDestinationViewController.h
@@ -1,3 +1,11 @@
+//
+//  UIStoryboardSegue+EffectiveDestinationViewController.h
+//  UIStoryboardSegue+EffectiveDestinationViewController
+//
+//  Created by Jonghwan Hyeon on 8/20/14.
+//  Copyright (c) 2014 Jonghwan Hyeon. All rights reserved.
+//
+
 #import <UIKit/UIKit.h>
 
 @interface UIStoryboardSegue (EffectiveDestinationViewController)
--- UIStoryboardSegue+EffectiveDestinationViewController.m
+++ UIStoryboardSegue+EffectiveDestinationViewController.m
@@ -1,3 +1,11 @@
+//
+//  UIStoryboardSegue+EffectiveDestinationViewController.m
+//  UIStoryboardSegue+EffectiveDestinationViewController
+//
+//  Created by Jonghwan Hyeon on 8/20/14.
+//  Copyright (c) 2014 Jonghwan Hyeon. All rights reserved.
+//
+
 #import "UIStoryboardSegue+EffectiveDestinationViewController.h"
 
 @implementation UIStoryboardSegue (EffectiveDestinationViewController)
